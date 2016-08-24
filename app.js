4f6103fd619ca40260e62e4c45c8f9752198bc97
Udit Kapahi <ukitkk@gmail.com>
2016-08-24 16:18:15 +0530

    Added few more features.

--- app.js
+++ app.js
@@ -4,14 +4,65 @@ var bodyParser=require('body-parser');
 var PAGE_ACCESS_TOKEN='EAAQ0rqoF1uABAIs4pYBVkZCySs4AHvF1SLtykll6B5NKdmx93mJmBWgO4qngbrFvxTLtFfwXyE5uHQtLUCTeJw8fQQAy36YFPua7YvgVECZBlvCVBDiDMViPeu6rLC3Jkp0ZBQ91ZARxAmSJHtPZBIr0ZBSYszGqNtvkfYTwQgZAAZDZD';
 var VALIDATION_TOKEN='phantasmist';
 var request=require('request');
+var firebase = require("firebase");
+var sleep = require('sleep');
+var config = {
+    apiKey: "AIzaSyDyoFVDbLmbvnp1t-QXfO4MzgefGgEaysE",
+    authDomain: "chatbot-d601d.firebaseapp.com",
+    databaseURL: "https://chatbot-d601d.firebaseio.com",
+    storageBucket: "chatbot-d601d.appspot.com",
+  };
+  firebase.initializeApp(config);
+  var rootRef = firebase.database().ref();
+
+// Attach an asynchronous callback to read the data at our posts reference
+function searchResponse(senderID,messageFromUser){
+  rootRef.child('questions').on("value", function(snapshot) {
+    var arrayFound = snapshot.val().filter(function(item) {
+      if(item.userMessage == messageFromUser){
+        sendTypingOn(senderID);
+        console.log(item.answer);
+        sendTextMessage(senderID,item.answer);
+        sendTypingOff(senderID);
+        return item.answer;
+      }else{
+        sendTypingOff(senderID);
+        return 'not found';
+      }
+    });
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
+
+function searchForPayload(senderID,message){
+  console.log("Searching for payload "+message);
+  rootRef.child('quick_replies').on("value", function(snapshot) {
+    console.log(snapshot.val());
+    var arrayFound = snapshot.val().filter(function(item) {
+      if(item.title == message){
+        sendTypingOn(senderID);
+        sleep.sleep(5);
+        console.log("Sending Payload "+item.payload);
+
+        fetchList(senderID,item.payload);
+        return item.payload;
+      }else{
+        sendTypingOff(senderID);
+        return 'not found';
+      }
+    });
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
+
 // Test
 app.get('/ping',function(req, res){
-  res.send('aah touch me not');
   fetchList("tezt");
   sendGenericMessage("test")
 });
 app.use(bodyParser.json());
-
 //TO VERIFY
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
@@ -23,10 +74,97 @@ app.get('/webhook', function(req, res) {
     res.sendStatus(403);
   }
 });
+setGreetingText();
+setPersistentMenu();
 
+function getUserNameForPersonalization(uid){
+  var url="https://graph.facebook.com/v2.6/"+uid+"?access_token="+PAGE_ACCESS_TOKEN;
+  var firstName='s';
+  request({
+    uri:url,
+    method:'GET'
+  },function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var obj = JSON.parse(body);
+      firstName=obj.first_name;
+      sendTextMessage(uid, "Hi "+firstName+" ! I am Soni, your personal advisor. \nI am here to help you find joy.");
+      console.log("userName "+obj.first_name);
+      sendYesNoQuickReplyButtons(uid);
+    }else {
+      console.error("Get User Details Error : "+response);
+    }
+  });
+
+}
 // all the stuff to be coded below
 /** Should do stuff that we want the app to do
 */
+//SET GREETING TEXT message
+
+function setGreetingText(){
+  console.log("Setting Greeeting text");
+
+  var jsonObject = {
+    setting_type: 'greeting',
+    thread_state:'existing_thread',
+    greeting: {
+      text: "Welocome to CRI Kasauli. You can ask us queries. We are in developement phase."
+    }
+  };
+  setThread(jsonObject);
+}
+
+// SET PERSISTENT MENU
+function setPersistentMenu(){
+  console.log("Setting Persistent Menu");
+
+  var jsonObject={
+  "setting_type" : "call_to_actions",
+  "thread_state" : "existing_thread",
+  "call_to_actions":[
+    {
+      "type":"postback",
+      "title":"Help",
+      "payload":"1"
+    },
+    {
+      "type":"postback",
+      "title":"Buy a property",
+      "payload":"2"
+    },
+    {
+      "type":"postback",
+      "title":"Filters",
+      "payload":"3"
+    }
+  ]
+};
+  setThread(jsonObject);
+}
+
+function setThread(jsonObject){
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+PAGE_ACCESS_TOKEN,
+      method: 'POST',
+      json: jsonObject
+
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var variabel = body;
+        // var messageId = body.message_id;
+          console.log(""+variabel);
+        // if (messageId) {
+        //   console.log("Successfully sent message with id %s to recipient %s",
+        //     messageId, recipientId);
+        // } else {
+        // console.log("Successfully called Send API for recipient %s",
+        //   recipientId);
+        // }
+      } else {
+        console.error("Error : "+response);
+      }
+    });
+  }
 
 // Receive text message
 app.post('/webhook', function (req, res) {
@@ -50,6 +188,8 @@ app.post('/webhook', function (req, res) {
           // receivedDeliveryConfirmation(messagingEvent);
         } else if (messagingEvent.postback) {
           receivedPostback(messagingEvent);
+        }else if (messagingEvent.read) {
+          receivedMessageRead(messagingEvent);
         } else {
           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
         }
@@ -81,11 +221,18 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
+  if (message.hasOwnProperty('quick_reply')) {
+      if(message.quick_reply.payload!='Yes-Property') {
+          searchForPayload(senderID,message);
+      };
+  }
+
   if (messageText) {
 
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
+
     switch (messageText) {
       case 'image':
         sendImageMessage(senderID);
@@ -104,91 +251,29 @@ function receivedMessage(event) {
       case 'receipt':
         sendReceiptMessage(senderID);
         break;
+      case 'read receipt':
+        sendReadReceipt(senderID);
+        break;
 
+      case 'typing on':
+        sendTypingOn(senderID);
+        break;
+
+      case 'typing off':
+        sendTypingOff(senderID);
+        break;
+      case 'Yes':
+          if(message.quick_reply.payload=='Yes-Property') {
+              sendCitySelectionButtons(senderID);
+          }
       default:
-        sendTextMessage(senderID, messageText);
+        searchResponse(senderID, messageText);
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
 
-// SEND MESSAGES OF DIFFERENt TYPES
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: ""+recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };
-  // console.log(messageData);
-  callSendAPI(messageData);
-}
-// What happens when user clicks on get started button
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  // The 'payload' param is a developer-defined field which is set in a postback
-  // button for Structured Messages.
-  var payload = event.postback.payload;
-
-  console.log("Received postback for user %d and page %d with payload '%s' " +
-    "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
-}
 
 function callSendAPI(messageData) {
   request({
@@ -210,9 +295,220 @@ function callSendAPI(messageData) {
         recipientId);
       }
     } else {
-      console.error("Error : "+response);
+      console.error("Error : "+response.message);
     }
   });
+}
+// SEND MESSAGES OF DIFFERENt TYPES
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: ""+recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function sendYesNoQuickReplyButtons(recipientId){
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text:"Are you looking to buy a house",
+      quick_replies:[{
+        content_type:"text",
+        title:"Yes",
+        payload:"Yes-Property"
+      },
+      {
+        content_type:"text",
+        title:"No",
+        payload:"No-Property"
+      }]
+    }
+  };
+  // console.log(messageData);
+  callSendAPI(messageData);
+}
+
+function sendCitySelectionButtons(recipientId){
+  console.log('Sending city buutons to '+recipientId);
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text:"Please choose a city",
+      quick_replies:[{
+        content_type:"text",
+        title:"Gurgaon",
+        payload:"1"
+      },
+      {
+        content_type:"text",
+        title:"Kolkata",
+        payload:"17"
+      },
+      {
+        content_type:"text",
+        title:"Mumbai",
+        payload:"13"
+      },
+      {
+        content_type:"text",
+        title:"Banglore",
+        payload:"10"
+      },
+      {
+        content_type:"text",
+        title:"Noida",
+        payload:"4"
+      }]
+    }
+  };
+  // console.log(messageData);
+  callSendAPI(messageData);
+}
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text:"pick a filter",
+      quick_replies:[{
+        content_type:"text",
+        title:"0l-40L",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        content_type:"text",
+        title:"40l-80L",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      },
+      {
+        content_type:"text",
+        title:"80l-1.2Cr",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      },
+      {
+        content_type:"text",
+        title:"1.2Cr-2cr",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      },
+      {
+        content_type:"text",
+        title:"1.2Cr-2cr",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }]
+    }
+  };
+  // console.log(messageData);
+  callSendAPI(messageData);
+}
+
+// What happens when user clicks on get started button
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+  //
+  // // The 'payload' param is a developer-defined field which is set in a postback
+  // // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  if (payload=='1') {
+      sendGenericMessage(senderID);
+  }else if (payload=='2') {
+    sendTextMessage(senderID,"finding");
+  }else if (payload=='3') {
+    sendTextMessage(senderID,"filtering");
+  }
+  else{
+    getUserNameForPersonalization(senderID);
+  }
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to
+  // let them know it was successful
+  // sendTextMessage(senderID, "Hi "+name+" ! How are you");
+}
+
+
+/*
+ * Message Read Event
+ *
+ * This event is called when a previously-sent message has been read.
+ *
+ */
+function receivedMessageRead(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+
+  // All messages before watermark (a timestamp) or sequence have been seen.
+  var watermark = event.read.watermark;
+  var sequenceNumber = event.read.seq;
+
+  console.log("Received message read event for watermark %d and sequence " +
+    "number %d", watermark, sequenceNumber);
+}
+/*
+ * Send a read receipt to indicate the message has been read
+ *
+ */
+function sendReadReceipt(recipientId) {
+  console.log("Sending a read receipt to mark message as seen");
+
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "mark_seen"
+  };
+
+  callSendAPI(messageData);
+}
+
+
+/*
+ * Turn typing indicator on
+ *
+ */
+function sendTypingOn(recipientId) {
+  console.log("Turning typing indicator on");
+
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "typing_on"
+  };
+
+  callSendAPI(messageData);
+}
+
+/*
+ * Turn typing indicator off
+ *
+ */
+function sendTypingOff(recipientId) {
+  console.log("Turning typing indicator off");
+
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "typing_off"
+  };
+
+  callSendAPI(messageData);
 }
 
 // my logic
@@ -224,12 +520,12 @@ function searchData(text){
 });
 }
 
-function fetchList(senderID){
+function fetchList(senderID,cityId){
   request( {
       uri: 'http://api.squareyards.com/SquareYards/site/mobile/projectinfocus',
       method: 'POST',
       json: {
-        "cityId": "1",
+        "cityId": ""+cityId,
         "pageno":"1"
       }
     }, function (error, response, body) {
