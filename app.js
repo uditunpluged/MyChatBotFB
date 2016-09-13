60af004243e3847235b36bbbbee9105b722a4379
Udit Kapahi <ukitkk@gmail.com>
2016-09-13 14:05:36 +0530

    minor changes.

--- app.js
+++ app.js
@@ -168,15 +168,15 @@ function searchForPayload(senderID, message, messagePayload) {
                     });
                 });
             } else if (item.payload.hasOwnProperty('bhkCount')) {
-                var bhkCountMap = new HashMap();
-                bhkCountMap.set('projectMaxPrice', usersMap.get(senderID).get("projectMaxPrice"));
-                bhkCountMap.set('projectMinPrice', usersMap.get(senderID).get("projectMinPrice"));
-                bhkCountMap.set('cityId', "" + usersMap.get(senderID).get("cityId"));
-                bhkCountMap.set('bhkCount', "" + item.payload.bhkCount);
-                usersMap.set(senderID, bhkCountMap);
-                console.log("BHK COUT : " + bhkCountMap.get("bhkCount"));
-
-                // sendTextMessage(senderID, "" + anotherNewMap.get("cityId"), function(data) {});
+                // var bhkCountMap = new HashMap();
+                // bhkCountMap.set('projectMaxPrice', usersMap.get(senderID).get("projectMaxPrice"));
+                // bhkCountMap.set('projectMinPrice', usersMap.get(senderID).get("projectMinPrice"));
+                // bhkCountMap.set('cityId', "" + usersMap.get(senderID).get("cityId"));
+                // bhkCountMap.set('bhkCount', "" + item.payload.bhkCount);
+                // usersMap.set(senderID, bhkCountMap);
+                // console.log("BHK COUT : " + bhkCountMap.get("bhkCount"));
+                //
+                // // sendTextMessage(senderID, "" + anotherNewMap.get("cityId"), function(data) {});
             } else if (item.payload === 'other') {
                 console.log("CITY ID", item.payload);
                 // sendPriceRangeButtons(senderID, item.payload);
